3c95fe9a02c81de98764c890a0428380f9911f7b
prebolla <prebolla@students.kennesaw.edu>
2019-03-08 20:00:42 -0500

    Swipe features + ingredients lists added

--- MainBuild/app/src/main/java/team1/intelligentcookingapp/favorites_page.kt
+++ MainBuild/app/src/main/java/team1/intelligentcookingapp/favorites_page.kt
@@ -3,6 +3,9 @@ package team1.intelligentcookingapp
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.GestureDetectorCompat
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
@@ -10,9 +13,13 @@ import android.widget.TextView
 
 class favorites_page : AppCompatActivity() {
 
+    private var gestureObject: GestureDetectorCompat? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favorites_page)
+        gestureObject = GestureDetectorCompat(this, LearnGesture())
+
 
         // Variable declarations ===================================================================
 
@@ -51,6 +58,7 @@ class favorites_page : AppCompatActivity() {
         grocery.setOnClickListener(View.OnClickListener {
             val intent2 = Intent(baseContext, grocery_page::class.java)
             startActivity(intent2)
+            overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
         })
 
         // Camera image listener -------------------------------------------------------------------
@@ -60,5 +68,32 @@ class favorites_page : AppCompatActivity() {
         // Onclick text listeners ==============================================================
         // =========================================================================================
 
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        this.gestureObject?.onTouchEvent(event)
+        return super.onTouchEvent(event)
+    }
+
+    internal inner class LearnGesture : GestureDetector.SimpleOnGestureListener() {
+
+        override fun onFling(
+            event1: MotionEvent, event2: MotionEvent,
+            velocityX: Float, velocityY: Float
+        ): Boolean {
+
+            if (event2.x > event1.x) {
+                val intentSwipeLeft = Intent(this@favorites_page, grocery_page::class.java)
+                finish()
+                startActivity(intentSwipeLeft)
+                overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+            }
+            else if (event2.x < event1.x) {
+                val intentSwipeRight = Intent(this@favorites_page, MainActivity::class.java)
+                finish()
+                startActivity(intentSwipeRight)
+            }
+            return true
+        }
     }
 }
