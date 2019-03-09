297a2db20a29c66c2f6481841b9ac4657e391ea7
JMurwin <jmurwin@students.kennesaw.edu>
2019-03-09 09:41:55 -0500

    - Database Test

--- JohnDev/app/src/main/java/john/murwin/databasetest/MainActivity.java
+++ JohnDev/app/src/main/java/john/murwin/databasetest/MainActivity.java
@@ -1,13 +1,17 @@
-package team1.intelligentcookingapp;
+package john.murwin.databasetest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    DatabaseHelper mainDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Creating the Database:
+        mainDB = new DatabaseHelper(this);
     }
 }
